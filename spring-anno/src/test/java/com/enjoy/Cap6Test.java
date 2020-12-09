1570ca8cf7b26a1f64a263d65f7d1db3c954f856
陈进 <1219650953@qq.com>
2020-12-09 17:21:49 +0800

    1

--- spring-anno/src/test/java/com/enjoy/Cap6Test.java
+++ spring-anno/src/test/java/com/enjoy/Cap6Test.java
@@ -15,6 +15,8 @@ public class Cap6Test {
         Object bean2 = app.getBean("jamesFactoryBean");//取Money
         System.out.println("bean的类型=" + bean1.getClass());
         System.out.println(bean1 == bean2);//使用isSingleton默认为true
+        Object bean = app.getBean("&jamesFactoryBean");//id前面加个&获取工厂bean本身对象
+        System.out.println("bean==="+bean);
         String[] beanDefinitionNames = app.getBeanDefinitionNames();
         for (String name : beanDefinitionNames) {
             System.out.println(name);
