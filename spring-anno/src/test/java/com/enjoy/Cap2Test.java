5e78c58e2adacb3965b30125c0e585bc4dd4bc21
陈进 <1219650953@qq.com>
2020-12-09 16:11:19 +0800

    1

--- spring-anno/src/test/java/com/enjoy/Cap2Test.java
+++ spring-anno/src/test/java/com/enjoy/Cap2Test.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import com.enjoy.cap2.config.Cap2MainConfig;
 
+/**
+ * 重要
+ * 测试的时候由于都是扫描的com.enjoy.cap2.config
+ * 所以Cap2MainConfig和Cap2MainConfig最好是注释掉其中一个，不然会混乱
+ */
 public class Cap2Test {
 	@Test
 	public void test01(){
